745006e228e8d9bf1c0564f655f4366a138ba5a5
Anderson-source-ai <testimonyanderson7@gmail.com>
2026-06-03 10:14:04 +0100

    Java Output

---
+++ Output.java
@@ -0,0 +1,13 @@
+public class Output{
+	public static void main(String[] args){
+		System.out.println("Hello everyone, this is my first Java code");
+		System.out.println("Learning Java is fun");
+		System.out.print("Here I am using using the print method. ");
+		System.out.print("This text will be appended. ");
+		System.out.printf("%nMercy Ben is %d years old",25);
+		System.out.printf("%nMy account ballance is %c%,.2f%n",'$',7654.98765);
+		System.out.printf("%s is a boy %n","Benjamin");
+		System.out.printf("Do you love Java? %b%n",true);
+		System.out.printf("Fuel price in nigeria have increased by %%80");
+	}
+}
